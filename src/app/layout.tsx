a92ff8bd9a3d0ed182fce91f08200638d30f355c
Rafael582480 <rafaelalvesnascimento919@gmail.com>
2026-03-21 14:40:56 -0300

    Arrumei mais algumas coisas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import {Header} from "@/src/components/header/Header";
 import { Footer } from "../components/footer/footer";
 import { config } from '@fortawesome/fontawesome-svg-core';
