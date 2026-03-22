9fc9143151fd3af9817f2272ef34a1fb2013ae97
Rafael582480 <rafaelalvesnascimento919@gmail.com>
2026-03-22 14:44:01 -0300

    fix: Responsividade adicionada a tela de Play

--- src/components/list_playlist/list_playlist.tsx
+++ src/components/list_playlist/list_playlist.tsx
@@ -31,8 +31,8 @@ export function List_playlist({ infos, course }: allVideos) {
 
     return (
         <>
-            <section className="w-screen flex justify-evenly">
-                <div className="flex flex-col w-1/3">
+            <section className="w-screen flex lg:flex-row flex-col-reverse md:flex-col-reverse justify-evenly">
+                <div className="flex flex-col w-full lg:w-1/3">
                     <div className="w-1/4 pt-2 flex flex-col gap-3 w-full">
                         <div className="bg-zinc-800 w-full text-white p-3">
                             <h1 className="font-bold text-lg">Aulas da Playlist</h1>
@@ -49,7 +49,7 @@ export function List_playlist({ infos, course }: allVideos) {
                         ))}
                     </ul>
                 </div>
-                <div className="w-4/5">
+                <div className="w-full lg:w-4/5">
                     <Watch_play
                         videoUrl={format_watch}
                     />
--- src/components/watch/watch.tsx
+++ src/components/watch/watch.tsx
@@ -10,7 +10,7 @@ export function Watch_play({ videoUrl }: watch_play) {
             src={videoUrl}
             referrerPolicy="strict-origin-when-cross-origin"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            className="h-150 w-full"
+            className="h-50  md:h-100 lg:h-150 w-full"
             allowFullScreen
             title="YouTube video player"
         />
