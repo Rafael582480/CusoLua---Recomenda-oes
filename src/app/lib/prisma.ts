defb188b9b5c7ca7d09d77aa3f78f275c59b6da0
Rafael582480 <rafaelalvesnascimento919@gmail.com>
2026-03-30 16:28:33 -0300

    Adivinha...

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -4,8 +4,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-// Para Prisma 6, NÃO passe opções no construtor
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
